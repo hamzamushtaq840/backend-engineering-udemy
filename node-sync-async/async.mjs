2e8b8e32f3fc9f24464f25929234ab28000e8544
Hamza Mushtaq <73603757+hamzamushtaq840@users.noreply.github.com>
2024-07-12 01:34:22 +0500

    initial commit

---
+++ node-sync-async/async.mjs
@@ -0,0 +1,7 @@
+import fs from "fs";
+
+console.log(1);
+
+fs.readFile("test.txt", console.log);
+
+console.log(2);
---
+++ node-sync-async/sync.mjs
@@ -0,0 +1,8 @@
+import fs from "fs";
+
+console.log(1);
+
+const res = fs.readFileSync("test.txt");
+console.log(res);
+
+console.log(2);
